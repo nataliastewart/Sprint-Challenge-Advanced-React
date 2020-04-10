5c14de0edf2f8af53fd8504be65cfb270a8ee118
nataliastewart <nataliastewart@users.noreply.github.com>
2020-04-10 13:50:25 -0400

    Hook darkMode working

--- client/src/components/Users.js
+++ client/src/components/Users.js
@@ -1,9 +1,18 @@
 import React from "react";
+import useDarkMode from "../hooks/useDarkMode";
 
 export default function Users(props) {
   console.log("PROPS on USER", props);
+
+  const [darkMode, setDarkMode] = useDarkMode("isDark");
+  const toggleMode = (e) => {
+    e.preventDefault();
+    setDarkMode(!darkMode);
+  };
+
   return (
     <div>
+      <div onClick={toggleMode}>DarkMode</div>
       {props.person.map((item) => (
         // console.log("item", item)
         <div key={item.id}>
